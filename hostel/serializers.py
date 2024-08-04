816d8ab76102d05af5780404453f8a658298cd63
Zawadirugenyi <zawadirugenyi@gmail.com>
2024-08-04 15:23:27 +0300

    testing

--- hostel/serializers.py
+++ hostel/serializers.py
@@ -8,6 +8,7 @@ class HostelSerializer(serializers.ModelSerializer):
         model = Hostel
         fields = '__all__'
 
+
 class RoomDescriptionSerializer(serializers.ModelSerializer):
     room_number = serializers.CharField(source='room.number', read_only=True)
     hostel_name = serializers.CharField(source='room.hostel.name')
@@ -15,6 +16,7 @@ class RoomDescriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = RoomDescription
         fields = ['room_number', 'hostel_name', 'sitting_room_image', 'bedroom_image', 'kitchen_image', 'bathroom_image', 'description', 'price']
+
 
 class RoomSerializer(serializers.ModelSerializer):
     hostel_name = serializers.CharField(source='hostel.name', read_only=True)
@@ -24,10 +26,12 @@ class RoomSerializer(serializers.ModelSerializer):
         model = Room
         fields = '__all__'
 
+
 class TenantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tenant
         fields = '__all__'
+        
 
 class StaffSerializer(serializers.ModelSerializer):
     class Meta:
